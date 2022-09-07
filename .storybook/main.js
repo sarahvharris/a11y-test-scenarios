a818d1ca69ccfd51a717af587179f07b02ca1484
Sarah Harris <svharris@Sarahs-MacBook-Pro.local>
2022-09-06 22:59:02 -0400

    chore(files): init SB files

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+};
